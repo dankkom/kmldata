e5b2db069d6116900deab28714a4f5a32cbd83f4
Daniel Komesu <danielkomesu@gmail.com>
2020-06-26 18:40:18 -0300

    Add functions in parser

--- kmldata/parser.py
+++ kmldata/parser.py
@@ -19,3 +19,37 @@ def get_doc(root):
 def get_folders(doc):
     for folder in doc.xpath("./t:Folder", namespaces=NS):
         yield folder
+
+
+def get_tree(doc):
+    folders = {folder.name: folder for folder in get_folders(doc)}
+    for folder_name in folders:
+        subfolders = get_tree(folders[folder_name])
+        if len(subfolders) > 0:
+            folders[folder_name] = subfolders
+    return folders
+
+
+def get_placemarks(doc):
+    for folder in doc.xpath("./t:Placemark", namespaces=NS):
+        yield folder
+
+
+def get_leafs(doc):
+    folders = {folder.name: folder for folder in get_folders(doc)}
+    for folder_name in folders:
+        subfolders = get_leafs(folders[folder_name])
+        if len(subfolders) > 0:
+            folders[folder_name] = subfolders
+    placemarks = list(get_placemarks(doc))
+    if placemarks:
+        folders["placemarks"] = placemarks
+    return folders
+
+
+def get_SimpleData(placemark):
+    data = {
+        simpledata.attrib.get("name"): simpledata.text
+        for simpledata in placemark.xpath(".//t:SimpleData", namespaces=NS)
+    }
+    return data
