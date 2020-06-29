6937312c115cdbb4db9021e78d20fee0feb4d05b
Daniel Komesu <danielkomesu@gmail.com>
2020-06-29 14:44:37 -0300

    Add type hints

--- kmldata/parser.py
+++ kmldata/parser.py
@@ -1,28 +1,31 @@
+from typing import Dict, Sequence, Union
+
 import numpy as np
 import pandas as pd
 from pykml import parser
+from pykml.factory import KML_ElementMaker as KML
 
 
 NS = {"t": "http://www.opengis.net/kml/2.2"}
 
 
-def read_kml(filepath):
+def read_kml(filepath: str) -> KML.kml:
     with open(filepath, "rb") as f:
         root = parser.parse(f).getroot()
     return root
 
 
-def get_doc(root):
+def get_doc(root: KML.kml) -> KML.Document:
     doc = root.xpath("./t:Document", namespaces=NS)[0]
     return doc
 
 
-def get_folders(doc):
+def get_folders(doc: KML.Document) -> KML.Folder:
     for folder in doc.xpath("./t:Folder", namespaces=NS):
         yield folder
 
 
-def get_tree(doc):
+def get_tree(doc: KML.Document) -> dict:
     folders = {folder.name: folder for folder in get_folders(doc)}
     for folder_name in folders:
         subfolders = get_tree(folders[folder_name])
@@ -34,12 +37,12 @@ def get_tree(doc):
     return folders
 
 
-def get_placemarks(doc):
+def get_placemarks(doc: KML.Document) -> KML.Placemark:
     for folder in doc.xpath("./t:Placemark", namespaces=NS):
         yield folder
 
 
-def get_SimpleData(placemark):
+def get_SimpleData(placemark: KML.Placemark) -> Dict[str, str]:
     data = {
         simpledata.attrib.get("name"): simpledata.text
         for simpledata in placemark.xpath(".//t:SimpleData", namespaces=NS)
@@ -47,12 +50,14 @@ def get_SimpleData(placemark):
     return data
 
 
-def get_description(placemark):
+def get_description(placemark: KML.Placemark) -> str:
     description = placemark.xpath(".//t:description", namespaces=NS)
     return "\n---\n".join(str(d) for d in description)
 
 
-def get_coordinates(placemark):
+def get_coordinates(
+        placemark: KML.Placemark
+) -> Dict[str, float]:
     if hasattr(placemark, "Point"):
         if hasattr(placemark.Point, "coordinates"):
             lon, lat, alt = placemark.Point.coordinates.text.split(",")
@@ -68,7 +73,9 @@ def get_coordinates(placemark):
     }
 
 
-def get_placemarks_data(placemarks):
+def get_placemarks_data(
+        placemarks: Sequence[KML.Placemark]
+) -> Dict[str, Union[str, float]]:
     for placemark in placemarks:
         yield dict(
             description=get_description(placemark),
@@ -77,7 +84,10 @@ def get_placemarks_data(placemarks):
         )
 
 
-def get_data(tree, folders=None):
+def get_data(
+        tree: KML,
+        folders: Sequence = None
+) -> Dict[str, Union[str, float]]:
     if folders is None:
         folders = tuple()
     for node in tree:
@@ -94,13 +104,13 @@ def get_data(tree, folders=None):
             )
 
 
-def get_dataframe_from_tree(tree):
+def get_dataframe_from_tree(tree: dict) -> pd.DataFrame:
     data = get_data(tree)
     df = pd.DataFrame.from_records(data)
     return df
 
 
-def get_dataframe_from_kml(filepath):
+def get_dataframe_from_kml(filepath: str) -> pd.DataFrame:
     root = read_kml(filepath)
     doc = get_doc(root)
     tree = get_tree(doc)
